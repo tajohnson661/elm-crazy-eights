247435fbd1cebf27f40644ba9f84c795ebd09bae
Tom Johnson <tom@toparstreet.com>
2016-12-02 15:12:12 -0800

    use Html.program and type annotate card deck

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,62 +1,141 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
+import Html.Events exposing (onClick)
+
 
 -- component import example
-import Components.Hello exposing ( hello )
+
+import Components.Hello exposing (hello)
+
+
+-- CARDS
+
+
+type alias Card =
+    ( Int, Char )
+
+
+faces : List Int
+faces =
+    [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ]
+
+
+suits : List Char
+suits =
+    [ 'H', 'D', 'C', 'S' ]
+
+
+allCards : List a -> List b -> List ( a, b )
+allCards faces suits =
+    case suits of
+        x :: xs ->
+            family faces x ++ allCards faces xs
+
+        _ ->
+            []
+
+
+family : List a -> b -> List ( a, b )
+family faces suit =
+    case faces of
+        x :: xs ->
+            (,) x suit :: family xs suit
+
+        _ ->
+            []
+
+
+deck : List Card
+deck =
+    allCards faces suits
+
 
 
 -- APP
-main : Program Never Int Msg
+
+
+main : Program Never Model Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
 
 
 -- MODEL
-type alias Model = Int
 
-model : number
-model = 0
+
+type alias Model =
+    Int
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
+
 
 
 -- UPDATE
-type Msg = NoOp | Increment
 
-update : Msg -> Model -> Model
+
+type Msg
+    = NoOp
+    | Increment
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        Increment ->
+            ( model + 1, Cmd.none )
+
 
 
 -- VIEW
 -- Html is defined as: elem [ attribs ][ children ]
 -- CSS can be applied via class names or inline style attrib
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
+    div [ class "container", style [ ( "margin-top", "30px" ), ( "text-align", "center" ) ] ]
+        [ -- inline CSS (literal)
+          div [ class "row" ]
+            [ div [ class "col-xs-12" ]
+                [ div [ class "jumbotron" ]
+                    [ img [ src "static/img/elm.jpg", style styles.img ] []
+                      -- inline CSS (via var)
+                    , hello model
+                      -- ext 'hello' component (takes 'model' as arg)
+                    , p [] [ text ("Elm Webpack Starter") ]
+                    , button [ class "btn btn-primary btn-lg", onClick Increment ]
+                        [ -- click handler
+                          span [ class "glyphicon glyphicon-star" ] []
+                          -- glyphicon
+                        , span [] [ text "FTW!" ]
+                        ]
+                    ]
+                ]
+            ]
         ]
-      ]
-    ]
-  ]
+
 
 
 -- CSS STYLES
+
+
 styles : { img : List ( String, String ) }
 styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+    { img =
+        [ ( "width", "33%" )
+        , ( "border", "4px solid #337AB7" )
+        ]
+    }
