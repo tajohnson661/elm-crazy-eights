12557fbac9d408b4510293466bed2472a2992854
Tom Johnson <tjohnson@toparstreet.com>
2016-12-09 14:26:28 -0800

    initial header and footer in UI, move to materialize instead of bootstrap

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -2,8 +2,8 @@ module View exposing (view)
 
 import Model exposing (Model, WhoseTurn(..))
 import Messages exposing (..)
-import Html exposing (Html, div, text, button, span, p, img, h3, h4, ul, li)
-import Html.Attributes exposing (class, style, src)
+import Html exposing (Html, div, text, button, span, p, img, h3, h4, h5, ul, li, nav, a, i, footer)
+import Html.Attributes exposing (class, style, src, href, id)
 import Html.Events exposing (onClick)
 import Cards exposing (Card, Suit, getSuitFromCard, getFaceFromCard)
 import GameLogic exposing (..)
@@ -17,22 +17,66 @@ import Dialog
 
 view : Model -> Html Msg
 view model =
+    div []
+        [ viewHeader model
+        , viewBody model
+        , viewFooter model
+        ]
+
+
+viewHeader : Model -> Html Msg
+viewHeader model =
+    nav [ class "light-blue lighten-1" ]
+        [ div [ class "nav-wrapper container" ]
+            [ a [ id "logo-container", href "#", class "brand-logo" ] [ text "Crazy Eights" ]
+            , ul [ class "right hide-on-med-and-down" ]
+                [ li []
+                    [ a [ onClick StartShuffle ] [ text "New game" ] ]
+                ]
+            , ul [ id "nav-mobile", class "side-nav" ]
+                [ li []
+                    [ a [ onClick StartShuffle ] [ text "New game" ] ]
+                ]
+            , a [ href "#", class "button-collapse" ]
+                [ i [ class "material-icons" ] [ text "menu" ] ]
+            ]
+        ]
+
+
+viewFooter : Model -> Html Msg
+viewFooter model =
+    footer [ class "page-footer orange" ]
+        [ div [ class "container" ]
+            [ div [ class "row" ]
+                [ div [ class "col l6 s12" ]
+                    [ h5 [ class "white-text" ] [ text "Company Bio" ]
+                    , p [ class "grey-text text-lighten-4" ] [ text "TJ Enterprises" ]
+                    ]
+                , div [ class "col l3 s12" ]
+                    [ text "hi" ]
+                , div [ class "col l3 s12" ]
+                    [ text "hi again" ]
+                ]
+            ]
+        , div [ class "footer-copyright" ]
+            [ div [ class "container" ]
+                [ span [] [ text "Made by me" ] ]
+            ]
+        ]
+
+
+viewBody : Model -> Html Msg
+viewBody model =
     let
         compareCard =
             List.head model.discardPile
     in
         div [ class "container", style [ ( "margin-top", "30px" ), ( "text-align", "center" ) ] ]
-            [ -- inline CSS (literal)
-              div [ class "row" ]
-                [ div [ class "col-xs-12" ]
+            [ viewDealerHand model.dealerHand
+            , div [ class "row" ]
+                [ div [ class "col s12" ]
                     [ div [ class "jumbotron" ]
                         [ h4 [] [ text model.message ]
-                        , button [ class "btn btn-primary btn-lg", onClick StartShuffle ]
-                            [ -- click handler
-                              span [ class "glyphicon glyphicon-star" ] []
-                              -- glyphicon
-                            , span [] [ text "New game" ]
-                            ]
                         , viewDiscardPile model.discardPile
                         , viewCurrentSuit model.currentSuit
                         ]
@@ -97,6 +141,11 @@ viewDrawCardButton model =
 
         _ ->
             empty
+
+
+viewDealerHand : List Card -> Html Msg
+viewDealerHand hand =
+    text "x"
 
 
 viewHand : List Card -> Maybe Card -> Suit -> WhoseTurn -> Html Msg
